881fa24a02f0f05b57ebfad03aa7076f5ed17917
Tessa Serdar <tessajeanserdar@gmail.com>
2016-05-16 11:42:43 -0700

    Sets layout to column grid and includes white/blue/grey material design styles

--- client/app/channel/channel.js
+++ client/app/channel/channel.js
@@ -2,9 +2,10 @@ angular.module('fickle.channel',[])
 .controller('channelController',function ($scope,Podcasts) {
   
   function init() {
+    $scope.loaded = false;
     Podcasts.getTags()
     .then(function (data){
-      console.log("we have the channels ",data)
+      $scope.loaded = true;
       $scope.podcastTags = data;
     })
   }
