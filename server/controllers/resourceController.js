7b88be4e3e28f0a45b35d360c62e83656ce1b450
Tessa Serdar <tessajeanserdar@gmail.com>
2016-05-16 11:41:37 -0700

    Added an id to queues produced by resource controller methods

--- server/controllers/resourceController.js
+++ server/controllers/resourceController.js
@@ -109,19 +109,20 @@ module.exports = {
         res.sendStatus(404);
     } 
    userPreferences.keywords = [userPreferences.resource.name];
-   db.cypherQuery("MATCH  (u:User {username:{username}})-[:HAS_SEEN]->(eps:Episode) WITH collect(distinct eps) as seenepisodes MATCH (e:Episode)--(resources:Resource)--(t:Tag) WHERE t.name IN {keywords} AND NOT e IN seenepisodes WITH t,resources,e, rand() AS  number return e.title as title,e.link,e.pubDate as date,resources.thumbnail as thumbnail,resources.url as showUrl,resources.name as showName,t.name as category ORDER BY number limit 10;",
+   db.cypherQuery("MATCH  (u:User {username:{username}})-[:HAS_SEEN]->(eps:Episode) WITH collect(distinct eps) as seenepisodes MATCH (e:Episode)--(resources:Resource)--(t:Tag) WHERE t.name IN {keywords} AND NOT e IN seenepisodes WITH t,resources,e, rand() AS  number return e.title as title,e.link,e.pubDate as date,resources.thumbnail as thumbnail,resources.url as showUrl,resources.name as showName,t.name,ID(e) as category ORDER BY number limit 10;",
        userPreferences,
        function(err, query) {
          if (err) { console.log(err) }
          formattedResponse = _.map(query.data,function(item){
            return {
              title: item[0],
-             episodeLink: item[1],
+             url: item[1],
              pubDate : item[2],
              thumbnail : item[3],
              itunesLink : item[4],
              resourcesName: item[5],
-             category : item[6]                                }
+             category : item[6],
+             id: item[7]                                }
          })
         res.send(formattedResponse)
        })
