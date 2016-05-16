fca4215d7a3efce917a6c0790649d7575e0d59b8
Tessa Serdar <tessajeanserdar@gmail.com>
2016-05-16 11:40:11 -0700

    Styles page to grey/white/blue material design

--- client/app/browse/browse.js
+++ client/app/browse/browse.js
@@ -1,19 +1,12 @@
 angular.module('fickle.browse', [])
 .controller('browseController', function (Browse,$scope, $http, $location, $window,Show) {
+   $scope.loaded = false;
     Browse.getPopularPodcasts()
     .then(function (data) {
+      $scope.loaded = true;
       $scope.browseResults = data;
     });
-
     $scope.getShow = function(show){
       Show.setShow(show)
     }
-
-   $scope.displayName = function(showName){
-    if(showName.length > 20) 
-      showName = showName.substring(0,10);
-      showName = showName.concat('...');
-      return showName;
-   }
- 
-});
+})
