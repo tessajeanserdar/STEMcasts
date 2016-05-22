ffbf556cc0bd9785c3f243ebc8f2fe926140b0b3
Tessa Serdar <tessajeanserdar@gmail.com>
2016-05-22 15:48:47 -0700

    Recovered 80% of styling my removed mobile display none

--- client/app/browse/browse.js
+++ client/app/browse/browse.js
@@ -1,13 +1,12 @@
 angular.module('fickle.browse', [])
-.controller('browseController', function (Browse,$scope, $http, $location, $window,Show) {  
-    Browse.getPopularPodcasts()
+.controller('browseController', function (Browse,$scope, $http, $location, $window,Show) {
+  $scope.loaded = false;
+  Browse.getPopularPodcasts()
     .then(function (data) {
+      $scope.loaded = true;
       $scope.browseResults = data;
-    });
-
-    $scope.getShow = function(show){
-      Show.setShow(show)
-    }
-
- 
-});
+  });
+  $scope.getShow = function(show){
+    Show.setShow(show)
+  }
+})
