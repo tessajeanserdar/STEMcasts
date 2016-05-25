d58a84f50fb2d83948e0bb4548ccab2eb6cf354d
Tessa Serdar <tessajeanserdar@gmail.com>
2016-05-24 19:31:29 -0700

    Fixed login error

--- client/app/login/login.js
+++ client/app/login/login.js
@@ -1,18 +1,23 @@
 angular.module('fickle.auth', [])
 
 .controller('AuthController', function ($scope, $http, $location, $window , Auth) {
-  $scope.signin = function (user) {
-    $scope.error = '';
-    if(!user) {
-      var userData = {
-        "username":$scope.username,
-        "password":$scope.password
-      }
-    } 
+  $scope.signin = function () {
+    // console.log(user)
+    // $scope.error = '';
+    // if(!user) {
+    //   var userData = {
+    //     "username":$scope.username,
+    //     "password":$scope.password
+    //   }
+    // } 
     Auth.signin(JSON.stringify($scope.user))
     .then(function(message){
-      window.localStorage.setItem('com.fickle', JSON.stringify(message[0]));
-      $location.path('/search');
+      if(message === "Wrong Input"){
+        alert("Thats not a valid user password combo")
+      } else {
+        window.localStorage.setItem('com.fickle', JSON.stringify(message[0]));
+        $location.path('/explore');
+      }
     })
     .catch(function (error) {
       console.error(error);
@@ -21,14 +26,13 @@ angular.module('fickle.auth', [])
 
   $scope.signup = function () {
     $scope.signUpError = '';
-    var userInfo = JSON.stringify($scope.user)
-    Auth.signup(userInfo)
+    Auth.signup($scope.user)
     .then(function(message){
       if(message==="existing"){
         alert("You already have an account. Please login.");
       } else {
         window.localStorage.setItem('com.fickle', JSON.stringify(message[0]));
-        $location.path('/search');
+        $location.path('/explore');
       }
     });
   };
